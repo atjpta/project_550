742f477326a68dd570c54dcb0a2b028577df730c
atjpta <caoducan351@gmail.com>
2023-01-26 23:03:34 +0700

    fix 3

--- client/config/index.js
+++ client/config/index.js
@@ -3,7 +3,7 @@ export default {
         apiimage: 'https://api-project-550.vercel.app/api/image',
         // apiimage: 'http://localhost:8088/api/image',
 
-        api: 'http://localhost:8088/api',
-        // api: 'https://api-project-550.vercel.app/api',
+        // api: 'http://localhost:8088/api',
+        api: 'https://api-project-550.vercel.app/api',
     },
 }
