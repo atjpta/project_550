e8dd420cc506647f7b12c0ec003a2aa82e1d2531
atjpta <caoducan351@gmail.com>
2023-03-16 04:02:11 +0700

    fix api image và chỉnh giao diện

--- client/config/index.js
+++ client/config/index.js
@@ -2,7 +2,7 @@ export default {
     url: {
         apiimage: 'https://khydqeqdyigehckvjaci.supabase.co/storage/v1/object/public/hmusic-files/image/',
 
-        api: 'http://localhost:8088/api',
-        // api: 'https://api-project-550.vercel.app/api',
+        // api: 'http://localhost:8088/api',
+        api: 'https://api-project-550.vercel.app/api',
     },
 }
